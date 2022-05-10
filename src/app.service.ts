de5f1de3765ef7d2563db26c5c51f659e6bd5d0c
_XiaoTian <istianlei@qq.com>
2022-05-10 11:47:28 +0800

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log("log: hello")
-    return 'Hello World!';
+    return 'Hello World! - fail';
   }
 }
